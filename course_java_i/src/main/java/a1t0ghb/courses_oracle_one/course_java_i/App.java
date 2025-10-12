9a74b3bc49c8a56efe7528268ef8af76fefe9373
a1t0git <github_ctc101@a1t0.aleeas.com>
2025-10-12 19:35:35 +0000

    feat: create Maven base project structure.

---
+++ course_java_i/src/main/java/a1t0ghb/courses_oracle_one/course_java_i/App.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/*
+ *  Path of 'package' corresponds to folder tree all the way down to CURRENT file, starting at base level of '<PROJECT_NAME>/src/main/java'.
+ *  - Ref.: 'https://medium.com/@pvprasanth474/understanding-the-maven-directory-structure-a-simple-guide-7c7d636ec614'.
+ */
+package a1t0ghb.courses_oracle_one.course_java_i;
+
+/**
+ *
+ * @author a1t0ghb
+ * IMPORTANT: class name SAME as filename.
+ */
+public class App {
+
+    public String getGreeting() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new App().getGreeting());
+    }
+
+}
+// EOF.
