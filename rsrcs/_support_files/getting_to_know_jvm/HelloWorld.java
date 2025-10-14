323f75bbb86b9c76f16a1e2bacea47c4df352f5e
a1t0git <github_ctc101@a1t0.aleeas.com>
2025-10-14 18:02:42 +0000

    chore: add files for testing JVM.

---
+++ rsrcs/_support_files/getting_to_know_jvm/HelloWorld.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/*
+ *  Getting to know JVM. Ref.: 'https://www.aluracursos.com/blog/paso-a-paso-jvm'.
+ *  - Basic exercise with only, plain-vanilla 'HelloWorld.java' file (without any Java build automation tool, such as Maven nor Gradle).
+ *  - Objective: to understand how 'JVM' works, to identify those files, by 'playing' with 'java', 'javac', and 'javap' commands.
+ */
+
+/**
+ *
+ * @author a1t0ghb
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+// EOF.
